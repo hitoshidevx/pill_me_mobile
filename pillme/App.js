3048c8e15455c70a3db83ec593e7366d1f864b76
Pedro Augusto <101184636+augustopdro@users.noreply.github.com>
2023-11-24 18:19:15 -0300

    Update App.js

--- pillme/App.js
+++ pillme/App.js
@@ -3,6 +3,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
 import { PaperProvider } from 'react-native-paper';
 
+{/*  Integrantes do grupo:
+RM 93442 - Gabriel Hitoshi Furone Yokogawa
+RM 93150 - Guilherme Martins Nascimento
+RM 93205 - Luis Fernando Nascimento de Oliveira
+RM 94990 - Pedro Augusto Pereira Viana
+RM 95800 - Yasmin Cabral Dias
+*/}
+
 
 export default function App() {
   return (
